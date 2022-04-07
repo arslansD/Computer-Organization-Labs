a5a23024f1780d02fa8ca17179aa2001fc0f9296
arslansD <55465158+arslansD@users.noreply.github.com>
2022-04-08 00:24:02 +0600

    Add files via upload

---
+++ Lab2/Lab2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(503, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "MessageBox.Show(string)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(503, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "MessageBox.Show(string, string)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(127, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(503, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "MessageBox.Show(string, string, MessageBoxButtons)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(127, 323);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(503, 56);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "MessageBox.Show(string, string, MessageBoxButtons, MessageBoxIcon)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(804, 133);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(295, 100);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Вызов диалогового окна";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 505);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
